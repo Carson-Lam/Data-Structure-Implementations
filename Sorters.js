41adf1a3b4dde25f5b2cb125274606df7f98b546
Carson Lam <lamcn51@gmail.com>
2025-01-19 01:35:53 -0500

    added mergeSort functionality, overlap handling

--- Sorters.js
+++ Sorters.js
@@ -2,6 +2,16 @@
  * Author: Carson Lam
  */
 
+let selectedSorter = 0;
+
+let stopSorting = false;
+
+let sorting = false;
+
+const title = document.querySelector('.typewriter .maintext');
+
+const output = document.querySelector('.outputText');
+
 class Node {
     constructor(item) {
         this.item = item;
@@ -9,13 +19,14 @@ class Node {
     }
 }
 
-
-
 class SortableList {
     constructor() {
         this.head = null;
         this.tail = null;
     }
+
+    //CREATE LINKED LIST
+    /******************************************************************************/
     add(item) {
         const n = new Node(item);
         n.item = item;
@@ -27,6 +38,8 @@ class SortableList {
         }
     }
 
+    //COMPARABLE METHODS
+    /******************************************************************************/
     compareTo(item1, item2) {
         if (typeof item1 === "string" || typeof item1 === "number") {
             return item1 < item2 ? -1 : item1 > item2 ? 1 : 0
@@ -46,10 +59,13 @@ class SortableList {
         m.item = tempItem;
     }
 
+
+    //QUICK SORT METHODS
+    /******************************************************************************/
     //Makes the partitions during the sort
     async partition(lo, hi) {
         //Empty list case
-        if (lo == null | hi == null) return null;
+        if (lo == null || hi == null || stopSorting) return null;
 
         //Establish low and hi of partition (Lomuto method)
         let i = lo;
@@ -59,9 +75,10 @@ class SortableList {
         let swapped = false;
 
         for (let j = lo; j != hi; j = j.next) {
+            if (stopSorting) return null;
             if (this.less(j.item, pivot.item)) {
-                if(i !== j){
-                    this.exch(i,j);
+                if (i !== j) {
+                    this.exch(i, j);
                     console.log(this.toString());
                     document.getElementById("outputText").innerHTML = this.toString();
                     await delay(500);
@@ -79,13 +96,26 @@ class SortableList {
         return (previous == lo) ? null : previous;
     }
 
-    async sort() {
-        this.sortNodes(this.head, this.tail);
+    async quickSort() {
+        sorting = true;
+        document.getElementById("outputText").innerHTML = "STARTING QUICK SORT";
+        console.log("STARTING QUICK SORT");
+        await delay(500);
+        document.getElementById("outputText").innerHTML = "Input: " + this.toString();
+        await delay(500);
+        await this.sortNodes(this.head, this.tail);
+        if (stopSorting) return null;
+        document.getElementById("outputText").innerHTML = "QUICK SORT FINISHED";
+        console.log("QUICK SORT FINISHED");
+        await delay(500);
+        document.getElementById("outputText").innerHTML = "Output: " + this.toString();
+        console.log("Output " + this.toString());
+        sorting = false;
     }
 
     async sortNodes(lo, hi) {
         //Base case
-        if (lo == null || lo == hi || lo == hi.next) return;
+        if (lo == null || lo == hi || lo == hi.next || stopSorting) return;
 
         //Partition and get pivot
         const pivot = await this.partition(lo, hi);
@@ -97,6 +127,101 @@ class SortableList {
         }
     }
 
+    //MERGE SORT METHODS
+    /******************************************************************************/
+    async mergeSort() {
+        sorting = true;
+        document.getElementById("outputText").innerHTML = "STARTING MERGE SORT";
+        await delay(500);
+        document.getElementById("outputText").innerHTML = "Input: " + this.toString();
+        await delay(500);
+        this.head = await this.mergeSorter(this.head);
+        if (stopSorting) return null;
+        document.getElementById("outputText").innerHTML = "MERGE SORT FINISHED";
+        console.log("MERGE SORT FINISHED");
+        await delay(500);
+        document.getElementById("outputText").innerHTML = "Output: " + this.toString();
+        console.log("Output: " + this.toString());
+        sorting = false;
+    }
+
+    async mergeSorter(head) {
+        if (stopSorting) return null;
+        //Print list to be sorted
+        if (!head || !head.next || stopSorting) {
+            document.getElementById("outputText").innerHTML = this.partialToString(head);
+            console.log(this.partialToString(head));
+            await delay(400);
+        } else {
+            document.getElementById("outputText").innerHTML = this.partialToString(head);
+            console.log(this.partialToString(head));
+            await delay(500);
+        }
+
+        //Base Case
+        if (!head || !head.next) return head;
+
+        const mid = await this.getMiddle(head);
+        const midNext = mid.next;
+        mid.next = null;
+
+        //Print two split halves
+        if (stopSorting) return null;
+        document.getElementById("outputText").innerHTML =
+            "Split: &nbsp;&nbsp;&nbsp;" + this.partialToString(head) + "&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;" + this.partialToString(midNext);
+        console.log(
+            "Split: &nbsp;&nbsp;&nbsp;" + this.partialToString(head) + "&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;" + this.partialToString(midNext)
+        );
+        await delay(800);
+
+        const left = await this.mergeSorter(head);
+        const right = await this.mergeSorter(midNext);
+        const leftStr = this.partialToString(left);
+        const rightStr = this.partialToString(right);
+        if (stopSorting) return null;
+        const result = await this.merge(left, right);
+
+        //Print merging halves 
+        if (stopSorting) return null;
+        document.getElementById("outputText").innerHTML =
+            "Merge: &nbsp;&nbsp;&nbsp;" + leftStr + "&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;" + rightStr + "&nbsp;&nbsp;&nbsp;=&nbsp;&nbsp;&nbsp;" + this.partialToString(result);
+        console.log(
+            "Merge: &nbsp;&nbsp;&nbsp;" + leftStr + "&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;" + rightStr + "&nbsp;&nbsp;&nbsp;=&nbsp;&nbsp;&nbsp;" + this.partialToString(result)
+        );
+        await delay(800);
+
+        //Print merged result
+        return result;
+    }
+
+    async getMiddle(head) {
+        if (head == null) return null;
+        let mid = head;
+        let end = head;
+        while (end.next && end.next.next) {
+            mid = mid.next;
+            end = end.next.next;
+        }
+        return mid;
+    }
+
+    async merge(left, right) {
+        //Base case
+        if (!left) return right;
+        if (!right) return left;
+
+        if (this.less(left.item, right.item)) {
+            left.next = await this.merge(left.next, right);
+            return left;
+        } else {
+            right.next = await this.merge(left, right.next);
+            return right;
+        }
+
+    }
+
+    //DISPLAY METHODS
+    /******************************************************************************/
     toString() {
         let s = "";
         let n = this.head;
@@ -109,6 +234,15 @@ class SortableList {
         }
         return s;
     }
+
+    partialToString(node) {
+        let s = "";
+        for (let n = node; n !== null; n = n.next) {
+            s += n.item + (n.next ? "->" : "");
+        }
+        return s;
+    }
+
     async visualize() {
         const outputElement = document.getElementById("outputText");
         outputElement.innerHTML = this.toString();
@@ -121,11 +255,6 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-/* 
-Test inputs:
-hungry
-3,4,1,9,5,3,8,4,5,3,12
-*/
 function convertStringToSortableList(str) {
     const list = new SortableList();
     for (const char of str) {
@@ -134,37 +263,131 @@ function convertStringToSortableList(str) {
     return list;
 }
 
+/* 
+Test inputs:
+hungry
+3,4,1,9,5,3,8,4,5,3,12
+*/
+
+
+// document.getElementById('inputForm').addEventListener('submit',  async function (event) {
+async function sortInput() {
+    stopSorting = false;
+    if (selectedSorter == 0) {
+
+        //QUICK SORT
+        /******************************************************************************/
+        try {
+            const input = document.getElementById('input').value;
+            if (typeof input === "string") {
+                //Number case
+                if (input.includes(',')) {
+                    const list = new SortableList();
+                    const numbers = input.split(','); //list becomes array of sirings [num1, num2, num3]
+                    for (const num of numbers) {
+                        const numberValue = parseFloat(num.trim()); //Convert to number
+                        if (!isNaN(numberValue)) list.add(numberValue); //Check if valid number and add to list
+                    }
+                    // await list.visualize();
+                    await list.quickSort();
+                }
+                //String case
+                else {
+                    const list = convertStringToSortableList(input);
+                    // await list.visualize();
+                    await list.quickSort();
+                }
+            }
+        } catch (error) {
+            alert("Cannot sort this input!")
+            console.error("Cannot sort this input: ", error);
+        }
+    } else if (selectedSorter == 1) {
+
+        //MERGE SORT
+        /******************************************************************************/
+        try {
+            const input = document.getElementById('input').value;
+            if (typeof input === "string") {
+                //Number case
+                if (input.includes(',')) {
+                    const list = new SortableList();
+                    const numbers = input.split(','); //list becomes array of sirings [num1, num2, num3]
+                    for (const num of numbers) {
+                        const numberValue = parseFloat(num.trim()); //Convert to number
+                        if (!isNaN(numberValue)) list.add(numberValue); //Check if valid number and add to list
+                    }
+                    await list.mergeSort();
+                }
+                //String case
+                else {
+                    const list = convertStringToSortableList(input);
+                    await list.mergeSort();
+                }
+            }
+        } catch (error) {
+            alert("Cannot sort this input!")
+            console.error("Cannot sort this input: ", error);
+        }
+    }
+}
 document.getElementById('inputForm').addEventListener('submit', async function (event) {
     event.preventDefault();
-    try {
-        const input = document.getElementById('input').value;
-        if (typeof input === "string") {
-            //Number case
-            if (input.includes(',')) {
-                const list = new SortableList();
-                const numbers = input.split(','); //list becomes array of sirings [num1, num2, num3]
-                for (const num of numbers) {
-                    const numberValue = parseFloat(num.trim()); //Convert to number
-                    if (!isNaN(numberValue)) list.add(numberValue); //Check if valid number and add to list
-                }
-                await list.visualize();
-                list.sort();
-                document.getElementById("outputText").innerHTML = list.toString();
-                console.log(list.toString());
-            } else {
-                const list = convertStringToSortableList(input);
-                await list.visualize();
-                list.sort();
-                document.getElementById("outputText").innerHTML = list.toString();
-                console.log(list.toString());
-            }
-        }
-    } catch (error) {
-        alert("Cannot sort this input!")
-        console.error("Cannot sort this input: ", error);
+    if (!sorting) await sortInput();
+    else return null;
+});
+
+document.getElementById('inputButton').addEventListener('click', async function (event) {
+    event.preventDefault();
+    if (!sorting) await sortInput();
+    else return null;
+});
+
+document.getElementById('qsButton').addEventListener('click', function (event) {
+    event.preventDefault();
+    if (selectedSorter == 0) {
+        return null;
+    } else {
+        //Reset Animation
+        title.style.animation = 'none';
+        title.textContent = 'QuickSorter';
+        void title.offsetHeight; //Force reflow
+        title.style.animation = '';
+
+        //Reset output box
+        stopSorting = true; //Triggers quitting condition in sorters
+        sorting = false; //Sets sorting to false in case sorter interrupted and cannot set false at the end
+        console.log("Sorting aborted!");
+        output.textContent = 'Sorted output';
+
+        //Set new sorter
+        selectedSorter = 0;
+        console.log(selectedSorter);
     }
 });
 
-document.getElementById('msButton').addEventListener('click', async function (event) {
+document.getElementById('msButton').addEventListener('click', function (event) {
     event.preventDefault();
+
+    if (selectedSorter == 1) {
+        return null;
+    } else {
+        //Reset animation
+        title.style.animation = 'none';
+        title.textContent = 'MergeSorter';
+        void title.offsetHeight; //Force reflow
+        title.style.animation = '';
+
+        //Reset output box
+        stopSorting = true; //Triggers quitting condition in sorters
+        sorting = false; //Sets sorting to false in case sorter interrupted and cannot set false at the end
+        console.log("Sorting aborted!");
+        output.textContent = 'Sorted output';
+
+        //Set new sorter
+        selectedSorter = 1;
+        console.log(selectedSorter);
+    }
+
+
 });
